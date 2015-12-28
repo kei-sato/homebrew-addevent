65f87168fb4e79dfdbc84208de091dac167956c5
Kei Sato <kei-sato@meeetol.com>
2015-12-28 17:18:47 +0900

    update formula

--- addevent.rb
+++ addevent.rb
@@ -1,9 +1,9 @@
 class Addevent < Formula
   desc "addevent - command to add events to Calendar (previously called iCal)"
   homepage "https://github.com/kei-sato/homebrew-addevent"
-  url "https://gist.githubusercontent.com/kei-sato/f55c4eafb5e689b7ce03/raw/dff045b3bf1488b10f8dacfecac540829707846e/addevent"
-  sha256 "abc65cf42951ba330816a66ec0e08363268f335d3cf6a9d508a23613684677c8"
-  version "1.0.1"
+  url "https://gist.githubusercontent.com/kei-sato/f55c4eafb5e689b7ce03/raw/32addd9feed46fed16fe1dc529033910d8f98761/addevent"
+  sha256 "95593d1236409c763be3284a87eedf7a0b1460260e2200947595a194899ffc43"
+  version "1.0.2"
 
   def install
     bin.install "addevent"
